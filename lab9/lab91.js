8699b482593e557d77158eb6efa3531e5b061d4f
andrés Amin Uribe <andresamin@gmail.com>
2015-04-11 07:57:49 -0500

    backup anterior repo
    backup anterior repo

---
+++ lab9/lab91.js
@@ -0,0 +1,10 @@
+// Creación del Callback
+function usarUnCallback(operacionesCallback){
+
+	 
+var i = prompt("Teclea un numero entero");
+var j = prompt("Teclea otro numero entero");
+ 
+document.write("La suma de las variables i + j da: " + (i + j));
+	operacionesCallback();
+}
